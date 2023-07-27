bb372488802cb6ef43ae78ccf1ad47082092c001
austinaeden <austin.mbogo@student.moringaschool.com>
2023-07-27 13:19:32 +0300

    done

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,37 +1,71 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import ItemForm from "./ItemForm";
+import Filter from "./Filter";
+import Item from "./Item";
 
-function ItemForm() {
-  const [name, setName] = useState("");
-  const [category, setCategory] = useState("Produce");
+function ShoppingList() {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [items, setItems] = useState([]);
+
+  function handleCategoryChange(category) {
+    setSelectedCategory(category);
+  }
+
+  //add useEffect hook
+  useEffect(() => {
+    fetch("http://localhost:3000/items")
+      .then((res) => res.json())
+      .then((items) => setItems(items));
+  }, []);
+
+  // add this callback function
+  function handleUpdateItem(updatedItem) {
+    const updatedItems = items.map((item) => {
+      if (item.id === updatedItem.id) {
+        return updatedItem;
+      } else {
+        return item;
+      }
+    });
+    setItems(updatedItems);
+  }
+
+  // add this function!
+  function handleAddItem(newItem) {
+    setItems([...items, newItem]);
+  }
+
+  function handleDeleteItem (deletedItem) {
+    const updatedItems = items.filter((item) => item.id !== deletedItem.id);
+    setItems(updatedItems);
+  }
+  
+
+  const itemsToDisplay = items.filter((item) => {
+    if (selectedCategory === "All") return true;
+
+    return item.category === selectedCategory;
+  });
 
   return (
-    <form className="NewItem">
-      <label>
-        Name:
-        <input
-          type="text"
-          name="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </label>
-
-      <label>
-        Category:
-        <select
-          name="category"
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
-        >
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
-      </label>
-
-      <button type="submit">Add to List</button>
-    </form>
+    <div className="ShoppingList">
+      <ItemForm onAddItem={handleAddItem} />
+      <Filter
+        category={selectedCategory}
+        onCategoryChange={handleCategoryChange}
+      />
+      <ul className="Items">
+        {itemsToDisplay.map((item) => (
+          <Item 
+          key={item.id} 
+          item={item} 
+          onUpdateItem={handleUpdateItem}
+          onDeleteItem={handleDeleteItem}
+          />
+        ))}
+      </ul>
+    </div>
   );
 }
 
-export default ItemForm;
+export default ShoppingList;
